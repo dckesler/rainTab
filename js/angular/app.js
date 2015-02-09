1fe45313aa77c3eb3541e6f490a4b101f4188413
Daniel Kesler <danielckesler@gmail.com>
2015-02-09 10:24:05 -0700

    Switched from react to angular.

---
+++ js/angular/app.js
@@ -0,0 +1,126 @@
+(function(){
+    angular
+    .module("clock", [])
+    .directive("dirClock", dirClock);
+    function dirClock($interval){
+        return {
+            restrict: "E",
+            templateUrl: "js/angular/dirClock.html",
+            controllerAs: "dirClock",
+            controller: function($scope, $interval){
+                var dirClock = this;
+
+                dirClock.setTime = setTime;
+                dirClock.setTime();
+                $interval(dirClock.setTime, 1000);
+
+                function setTime(){
+                    var clockTotal = new Date();
+                    var clockers = {
+                        sec: clockTotal.getSeconds(),
+                        min: clockTotal.getMinutes(),
+                        hour: clockTotal.getHours(),
+                        month: clockTotal.getMonth(),
+                        day: clockTotal.getDate(),
+                        ofWeek: clockTotal.getDay()
+                    };
+                    switch (clockers.month){
+                        case "00":
+                        case 00:
+                            clockers.month = "January";
+                            break;
+                        case "01":
+                        case 01:
+                            clockers.month = "February";
+                            break;
+                        case "02":
+                        case 02:
+                            clockers.month = "March";
+                            break;
+                        case "03":
+                        case 03:
+                            clockers.month = "April";
+                            break;
+                        case "04":
+                        case 04:
+                            clockers.month = "May";
+                            break;
+                        case "05":
+                        case 05:
+                            clockers.month = "June";
+                            break;
+                        case "06":
+                        case 06:
+                            clockers.month = "July";
+                            break;
+                        case "07":
+                        case 07:
+                            clockers.month = "August";
+                            break;
+                        case "08":
+                        case 08:
+                            clockers.month = "September";
+                            break;
+                        case "09":
+                        case 09:
+                            clockers.month = "October";
+                            break;
+                        case "10":
+                        case 10:
+                            clockers.month = "November";
+                            break;
+                        case "11":
+                        case 11:
+                            clockers.month = "December";
+                            break;
+                    }
+                    switch (clockers.ofWeek){
+                        case 1:
+                            clockers.ofWeek = "Monday";
+                            break;
+                        case 2:
+                            clockers.ofWeek = "Tuesday";
+                            break;
+                        case 3:
+                            clockers.ofWeek = "Wednesday";
+                            break;
+                        case 4:
+                            clockers.ofWeek = "Thursday";
+                            break;
+                        case 5:
+                            clockers.ofWeek = "Friday";
+                            break;
+                        case 6:
+                            clockers.ofWeek = "Saturday";
+                            break;
+                        case 0:
+                            clockers.ofWeek = "Sunday";
+                            break;
+                    }
+                    clockers.hour = clockers.hour % 12;
+
+                    for(var key in clockers) {
+                        if ((clockers[key] + "").length === 1 && key !== "day" && key !== "ofWeek" && key !== "hour") {
+                            clockers[key] = "0" + clockers[key];
+                        }
+                    }
+                    dirClock.sec = clockers.sec;
+                    dirClock.min =  clockers.min;
+                    dirClock.hour = clockers.hour;
+                    dirClock.month = clockers.month;
+                    dirClock.day = clockers.day;
+                    dirClock.ofWeek = clockers.ofWeek;
+                }
+
+
+            },
+            link: function(scope){
+                var clockTotal = new Date();
+
+
+
+                
+            }
+        }
+    }
+})();
---
+++ js/rainStuff.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    var canvas = document.getElementById("night");
+    canvas.width = document.body.clientWidth;
+    canvas.height = window.innerHeight - 75;
+    var rains = canvas.width * canvas.height;
+    window.onresize = function(){
+        canvas.width = document.body.clientWidth;
+        canvas.height = window.innerHeight - 75;
+        rains = canvas.width * canvas.height;
+        console.log(rains);
+    };
+    var context = canvas.getContext('2d');
+    var RainDrop = function(width){
+        this.x = Math.floor(Math.random() * width);
+        this.y = 0;
+        this.size = Math.round((Math.random() * 25) + 5);
+        this.drop = (Math.floor((Math.random() * 2) + 12));
+        this.drop += this.drop * this.size/14;
+        this.d = (Math.floor(Math.random() * 2) -1);
+        this.delay = Math.floor(Math.random() * 6000);
+    };
+    var rainDropGroups = [];
+    function makeRainGroup (){
+        var group = [];
+        for(var i = 0; i < rains/2600; i++){
+            group.push(new RainDrop(canvas.width))
+        }
+        rainDropGroups.push(group);
+    }
+    makeRainGroup();
+    function go(){
+        rainDropGroups.forEach(function(elem, index, array){
+            elem.forEach(function(element){
+                setTimeout(function(){
+                    rain(element);
+                }, element.delay)
+            });
+        });
+
+        function rain(element) {
+            setInterval(function(){
+                var oldElement = {
+                    x: element.x,
+                    y: element.y
+                };
+                element.x -= element.d;
+                element.y += element.drop;
+                if( element.x < 0 || element.x > canvas.width || element.y > canvas.height){
+                    element.x = Math.floor(Math.random() * canvas.width);
+                    element.y = 0;
+                }
+                drawRain(element, oldElement);
+            }, 30);
+
+        }
+        function drawRain(raindrop, oldElement){
+            context.fillStyle = "HSL(33, 38%, 8%)";
+            context.fillRect(oldElement.x - 1, oldElement.y - 1, 4, raindrop.size + 2);
+            var grd = context.createRadialGradient(250, 550, 140, 250, 600, 650);
+            grd.addColorStop(0, 'HSL(33, 38%, 24%)');
+            grd.addColorStop(0.1, 'HSL(33, 38%, 20%)');
+            grd.addColorStop(0.2, 'HSL(33, 38%, 16%)');
+            grd.addColorStop(1, 'HSL(33, 38%, 15%)');
+            context.fillStyle = grd;
+            context.fillRect(raindrop.x, raindrop.y, 2, raindrop.size);
+        }
+    }
+    function lampFlicker(){
+        $(".light").fadeTo(100, .01, function(){
+            $(".light").fadeTo(100, .03)
+        });
+        $(".glass").fadeTo(100, .5, function(){
+            $(".glass").fadeTo(100,.8, function(){
+                setTimeout(lampFlicker, (Math.random() * 5000 + 2000))
+            })
+        });
+    }
+    go();
+    lampFlicker();
+    function fadeIn(){
+        $("#clock").fadeTo(6000,.3)
+    }
+    fadeIn();
+});
